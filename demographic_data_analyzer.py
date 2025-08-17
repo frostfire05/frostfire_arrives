ad64d5e02d3310305c339194f48f4d354b4bdf74
Vibhan Dutta <bivandatta07@gmail.com>
2025-08-17 22:10:05 +0530

    Update demographic_data_analyzer.py

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 
 def calculate_demographic_data(print_data=True):
-    # Read data from file
     df = pd.read_csv('adult.data.csv')
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
     race_count = df['race'].value_counts()
@@ -12,9 +11,6 @@ def calculate_demographic_data(print_data=True):
 
     # What is the percentage of people who have a Bachelor's degree?
     percentage_bachelors = round((df['education']=='Bachelors').mean()*100, 1)
-
-    # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
-    # What percentage of people without advanced education make more than 50K?
     
     # with and without `Bachelors`, `Masters`, or `Doctorate`
     higher_education = df[df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
@@ -38,8 +34,6 @@ def calculate_demographic_data(print_data=True):
     highest_earning_country_percentage = round((country_salary_counts/country_counts*100).max(), 1)
     highest_earning_country = (country_salary_counts/country_counts).idxmax()
     
-
-    # Identify the most popular occupation for those who earn >50K in India.
     top_IN_occupation = df[(df['native-country']=='India')&(df['salary']=='>50K')]['occupation'].value_counts().idxmax()
 
     if print_data:
